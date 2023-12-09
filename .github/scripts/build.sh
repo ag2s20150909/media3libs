6d2373e4b2fad37d74d367ddb490cabd0f104352
ag2s20150909 <ag2s20150909@gmail.com>
2023-12-10 02:38:43 +0800

    ggg

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,5 +8,7 @@ git clone --depth=1 -b release  https://github.com/androidx/media
 cd media
 MEDIA3_PATH="$(pwd)"
 
+chmod +x ${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
+chmod +x ${ROOT_DIR}/.github/scripts/build-av1.sh
 ${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
 ${ROOT_DIR}/.github/scripts/build-av1.sh
