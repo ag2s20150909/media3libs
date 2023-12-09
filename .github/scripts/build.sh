e25811d207e196f21d5e1a4f6eb95e3be78361d2
ag2s20150909 <ag2s20150909@gmail.com>
2023-12-10 02:42:42 +0800

    grrt

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,11 +4,13 @@ set -eux
 echo $ANDROID_NDK_HOME
 echo $NDK_PATH
 
-git clone --depth=1 -b release  https://github.com/androidx/media
-cd media
-MEDIA3_PATH="$(pwd)"
-
 chmod +x ${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
 chmod +x ${ROOT_DIR}/.github/scripts/build-av1.sh
+
+git clone --depth=1 -b release  https://github.com/androidx/media
+cd media
+export MEDIA3_PATH="$(pwd)"
+
+
 ${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
 ${ROOT_DIR}/.github/scripts/build-av1.sh
